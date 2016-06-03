4d92753a0bcbc180d5f90a1377a0c1bc927511a7
yumetodo <yume-wikijp@live.jp>
2016-06-03 14:07:48 +0900

    fix compile error

--- string_split.hpp
+++ string_split.hpp
@@ -1,4 +1,8 @@
+﻿#include <string>
+#include <vector>
+#include <type_traits>
 namespace detail {
+	using std::vector;
 	namespace type_traits {
 		//
 		// is_char_type
@@ -28,49 +32,46 @@ namespace detail {
 		struct is_stl_string<std::basic_string<CharType>> : std::integral_constant<bool, is_char_type<std::remove_cv_t<CharType>>::value> {};
 	}
 	template<typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	struct Split_helper_index;
+	struct split_helper_index;
 	template<typename CharType>
-	struct Split_helper_index<CharType, true, false, false> { 
+	struct split_helper_index<CharType, true, false, false> { 
 		using char_type = CharType;
 		CharType delim; std::size_t index;
 	};
 	template<typename CStr>
-	struct Split_helper_index<CStr, false, true, false> {
+	struct split_helper_index<CStr, false, true, false> {
 		using char_type = std::remove_pointer_t<CStr>;
 		const char_type* delim; std::size_t index;
 	};
 	template<typename StlString>
-	struct Split_helper_index<StlString, false, false, true> {
+	struct split_helper_index<StlString, false, false, true> {
 		using char_type = typename StlString::value_type;
 		std::basic_string<char_type> delim; std::size_t index;
 	};
 	template<typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	struct Split_helper;
+	struct split_helper;
 	template<typename CharType>
-	struct Split_helper<CharType, true, false, false> {
+	struct split_helper<CharType, true, false, false> {
 		using char_type = CharType;
 		CharType delim;
-		constexpr Split_helper_index<CharType, true, false, false> operator[](std::size_t n) const noexcept { return{ delim, n }; }
-		constexpr Split_helper_index<CharType, true, false, false> operator[](CsvParseLevel n) const noexcept { return this->operator[](static_cast<std::size_t>(n)); }
+		constexpr split_helper_index<CharType, true, false, false> operator[](std::size_t n) const noexcept { return{ delim, n }; }
 	};
 	template<typename CStr>
-	struct Split_helper<CStr, false, true, false> {
+	struct split_helper<CStr, false, true, false> {
 		using char_type = std::remove_pointer_t<CStr>;
 		const char_type* delim;
-		constexpr Split_helper_index<CStr, false, true, false> operator[](std::size_t n) const noexcept { return{ delim, n }; }
-		constexpr Split_helper_index<CStr, false, true, false> operator[](CsvParseLevel n) const noexcept { return this->operator[](static_cast<std::size_t>(n)); }
+		constexpr split_helper_index<CStr, false, true, false> operator[](std::size_t n) const noexcept { return{ delim, n }; }
 	};
 	template<typename StlString>
-	struct Split_helper<StlString, false, false, true> {
+	struct split_helper<StlString, false, false, true> {
 		using char_type = typename StlString::value_type;
 		std::basic_string<char_type> delim;
-		constexpr Split_helper_index<StlString, false, true, false> operator[](std::size_t n) const noexcept { return{ std::move(delim), n }; }
-		constexpr Split_helper_index<StlString, false, true, false> operator[](CsvParseLevel n) const noexcept { return this->operator[](static_cast<std::size_t>(n)); }
+		constexpr split_helper_index<StlString, false, true, false> operator[](std::size_t n) const noexcept { return{ std::move(delim), n }; }
 	};
 	//区切り文字1文字の時
 	template<typename CharType, typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	auto operator| (const std::basic_string<CharType>& str, const Split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>& info) 
-		-> std::enable_if_t<is_single_char && !is_c_str && !is_stl_string && std::is_same<CharType, typename Split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, std::basic_string<CharType>>
+	auto operator| (const std::basic_string<CharType>& str, const split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>& info) 
+		-> std::enable_if_t<is_single_char && !is_c_str && !is_stl_string && std::is_same<CharType, typename split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, std::basic_string<CharType>>
 	{
 		std::size_t pre = 0, pos = 0;
 		for (size_t i = 0; i < info.index + 1; ++i) {
@@ -81,8 +82,8 @@ namespace detail {
 	}
 	//区切り文字複数の時
 	template<typename CharType, typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	auto operator| (const std::basic_string<CharType>& str, const Split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>& info)
-		-> std::enable_if_t<!is_single_char && (is_c_str || is_stl_string) && std::is_same<CharType, typename Split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, std::basic_string<CharType>>
+	auto operator| (const std::basic_string<CharType>& str, const split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>& info)
+		-> std::enable_if_t<!is_single_char && (is_c_str || is_stl_string) && std::is_same<CharType, typename split_helper_index<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, std::basic_string<CharType>>
 	{
 		std::size_t pre = 0, pos = 0;
 		for (size_t i = 0; i < info.index + 1; ++i) {
@@ -93,8 +94,8 @@ namespace detail {
 	}
 	//区切り文字1文字の時
 	template<typename CharType, typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	auto operator| (const std::basic_string<CharType>& str, const Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
-		-> std::enable_if_t<is_single_char && !is_c_str && !is_stl_string && std::is_same<CharType, typename Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
+	auto operator| (const std::basic_string<CharType>& str, const split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
+		-> std::enable_if_t<is_single_char && !is_c_str && !is_stl_string && std::is_same<CharType, typename split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
 	{
 		vector<std::basic_string<CharType>> re;
 		size_t current = 0;
@@ -107,14 +108,14 @@ namespace detail {
 	}
 	//区切り文字複数の時
 	template<typename CharType, typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	auto operator| (const std::basic_string<CharType>& str, const Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
-		-> std::enable_if_t<!is_single_char && (is_c_str || is_stl_string) && std::is_same<CharType, typename Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
+	auto operator| (const std::basic_string<CharType>& str, const split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
+		-> std::enable_if_t<!is_single_char && (is_c_str || is_stl_string) && std::is_same<CharType, typename split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
 	{
 		vector<std::basic_string<CharType>> re;
 		size_t current = 0;
 		for (
 			size_t found = str.find_first_of(info.delim, current); 
-			current != basic_string<CharType>::npos;
+			current != std::basic_string<CharType>::npos;
 			current = str.find_first_not_of(str[found], found + 1), found = str.find_first_of(info.delim, current)
 		) {
 			if (re.capacity() < re.size() + 1) re.reserve((std::numeric_limits<std::size_t>::max() / 2 < re.size()) ? std::numeric_limits<std::size_t>::max() : re.size() * 2);
@@ -125,8 +126,8 @@ namespace detail {
 	}
 	//区切り文字1文字の時
 	template<typename CharType, typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	auto operator| (std::basic_string<CharType>&& str, const Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
-		-> std::enable_if_t<is_single_char && !is_c_str && !is_stl_string && std::is_same<CharType, typename Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
+	auto operator| (std::basic_string<CharType>&& str, const split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
+		-> std::enable_if_t<is_single_char && !is_c_str && !is_stl_string && std::is_same<CharType, typename split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
 	{
 		vector<std::basic_string<CharType>> re;
 		size_t current = 0;
@@ -140,8 +141,8 @@ namespace detail {
 	}
 	//区切り文字複数の時
 	template<typename CharType, typename DelimType, bool is_single_char, bool is_c_str, bool is_stl_string>
-	auto operator| (std::basic_string<CharType>&& str, const Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
-		-> std::enable_if_t<!is_single_char && (is_c_str || is_stl_string) && std::is_same<CharType, typename Split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
+	auto operator| (std::basic_string<CharType>&& str, const split_helper<DelimType, is_single_char, is_c_str, is_stl_string>& info)
+		-> std::enable_if_t<!is_single_char && (is_c_str || is_stl_string) && std::is_same<CharType, typename split_helper<DelimType, is_single_char, is_c_str, is_stl_string>::char_type>::value, vector<std::basic_string<CharType>>>
 	{
 		vector<std::basic_string<CharType>> re;
 		size_t current = 0;
@@ -159,8 +160,8 @@ namespace detail {
 	}
 }
 template<typename CharType, std::enable_if_t<detail::type_traits::is_char_type<CharType>::value, std::nullptr_t> = nullptr>
-detail::Split_helper<CharType, true, false, false> Split(CharType delim) noexcept { return{ delim }; }
+detail::split_helper<CharType, true, false, false> split(CharType delim) noexcept { return{ delim }; }
 template<typename CStr, std::enable_if_t<detail::type_traits::is_c_str<CStr>::value, std::nullptr_t> = nullptr>
-detail::Split_helper<CStr, false, true, false> Split(CStr delim) noexcept { return{ delim }; }
+detail::split_helper<CStr, false, true, false> split(CStr delim) noexcept { return{ delim }; }
 template<typename StlString, std::enable_if_t<detail::type_traits::is_stl_string<StlString>::value, std::nullptr_t> = nullptr>
-detail::Split_helper<StlString, false, false, true> Split(StlString delim) noexcept { return{ std::move(delim }; }
+detail::split_helper<StlString, false, false, true> split(StlString delim) noexcept { return{ std::move(delim) }; }
