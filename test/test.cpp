d1f5ef1e7d4f9a45e98f8c1e752a0c0dd94e8df7
yumetodo <yume-wikijp@live.jp>
2016-07-07 12:10:55 +0900

    add test case

--- test/test.cpp
+++ test/test.cpp
@@ -13,6 +13,9 @@ IUTEST_TEST(ExtractBySingeCharTest, type_char) {
 	IUTEST_ASSERT_EQ("na", s | split(' ')[1]);
 	IUTEST_ASSERT_EQ("world!", s | split(' ')[2]);
 	IUTEST_EXPECT_THROW(s | split(' ')[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(' ')[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(' ')[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(' ')[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 IUTEST_TEST(ExtractBySingeCharTest, type_wchar_t) {
 	std::wstring s = L"arikitari na world!";
@@ -20,6 +23,9 @@ IUTEST_TEST(ExtractBySingeCharTest, type_wchar_t) {
 	IUTEST_ASSERT_EQ(L"na", s | split(L' ')[1]);
 	IUTEST_ASSERT_EQ(L"world!", s | split(L' ')[2]);
 	IUTEST_EXPECT_THROW(s | split(L' ')[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(L' ')[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(L' ')[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(L' ')[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 IUTEST_TEST(ExtractBySingeCharTest, type_char16_t) {
 	std::u16string s = u"arikitari na world!";
@@ -27,6 +33,9 @@ IUTEST_TEST(ExtractBySingeCharTest, type_char16_t) {
 	IUTEST_ASSERT_EQ(u"na", s | split(u' ')[1]);
 	IUTEST_ASSERT_EQ(u"world!", s | split(u' ')[2]);
 	IUTEST_EXPECT_THROW(s | split(u' ')[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(u' ')[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(u' ')[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(u' ')[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 IUTEST_TEST(ExtractBySingeCharTest, type_char32_t) {
 	std::u32string s = U"arikitari na world!";
@@ -34,6 +43,9 @@ IUTEST_TEST(ExtractBySingeCharTest, type_char32_t) {
 	IUTEST_ASSERT_EQ(U"na", s | split(U' ')[1]);
 	IUTEST_ASSERT_EQ(U"world!", s | split(U' ')[2]);
 	IUTEST_EXPECT_THROW(s | split(U' ')[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(U' ')[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(U' ')[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(U' ')[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 IUTEST_TEST(ExtractByCStrTest, type_char) {
 	std::string s = "arikitari na_world!";
@@ -42,6 +54,9 @@ IUTEST_TEST(ExtractByCStrTest, type_char) {
 	IUTEST_ASSERT_EQ("na", s | split(delem)[1]);
 	IUTEST_ASSERT_EQ("world!", s | split(delem)[2]);
 	IUTEST_EXPECT_THROW(s | split(delem)[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 IUTEST_TEST(ExtractByCStrTest, type_wchar_t) {
 	std::wstring s = L"arikitari na_world!";
@@ -50,6 +65,9 @@ IUTEST_TEST(ExtractByCStrTest, type_wchar_t) {
 	IUTEST_ASSERT_EQ(L"na", s | split(delem)[1]);
 	IUTEST_ASSERT_EQ(L"world!", s | split(delem)[2]);
 	IUTEST_EXPECT_THROW(s | split(delem)[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 IUTEST_TEST(ExtractByCStrTest, type_char16_t) {
 	std::u16string s = u"arikitari na_world!";
@@ -58,6 +76,9 @@ IUTEST_TEST(ExtractByCStrTest, type_char16_t) {
 	IUTEST_ASSERT_EQ(u"na", s | split(delem)[1]);
 	IUTEST_ASSERT_EQ(u"world!", s | split(delem)[2]);
 	IUTEST_EXPECT_THROW(s | split(delem)[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 IUTEST_TEST(ExtractByCStrTest, type_char32_t) {
 	std::u32string s = U"arikitari na_world!";
@@ -66,31 +87,50 @@ IUTEST_TEST(ExtractByCStrTest, type_char32_t) {
 	IUTEST_ASSERT_EQ(U"na", s | split(delem)[1]);
 	IUTEST_ASSERT_EQ(U"world!", s | split(delem)[2]);
 	IUTEST_EXPECT_THROW(s | split(delem)[3], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[5], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[3250], std::out_of_range);
+	IUTEST_EXPECT_THROW(s | split(delem)[std::numeric_limits<std::size_t>::max()], std::out_of_range);
 }
 
 IUTEST_TEST(SplitBySingeCharTest, type_char) {
-	const std::string s = "arikitari na world!";
-	const std::vector<std::string> re1 = { "arikitari", "na", "world!" };
-	const auto re2 = s | split(' ');
-	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
+	const std::string s1 = "arikitari na world!";
+	const std::vector<std::string> re1_1 = { "arikitari", "na", "world!" };
+	const auto re1_2 = s1 | split(' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
+	const std::string s2 = "ありきたりな 世界";
+	const std::vector<std::string> re2_1 = { "ありきたりな", "世界" };
+	const auto re2_2 = s1 | split(' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
 }
 IUTEST_TEST(SplitBySingeCharTest, type_wchar_t) {
-	const std::wstring s = L"arikitari na world!";
-	const std::vector<std::wstring> re1 = { L"arikitari", L"na", L"world!" };
-	const auto re2 = s | split(L' ');
-	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
+	const std::wstring s1 = L"arikitari na world!";
+	const std::vector<std::wstring> re1_1 = { L"arikitari", L"na", L"world!" };
+	const auto re1_2 = s1 | split(L' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
+	const std::wstring s2 = L"ありきたりな 世界";
+	const std::vector<std::wstring> re2_1 = { L"ありきたりな", L"世界" };
+	const auto re2_2 = s1 | split(L' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
 }
 IUTEST_TEST(SplitBySingeCharTest, type_char16_t) {
-	const std::u16string s = u"arikitari na world!";
-	const std::vector<std::u16string> re1 = { u"arikitari", u"na", u"world!" };
-	const auto re2 = s | split(u' ');
-	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
+	const std::u16string s1 = u"arikitari na world!";
+	const std::vector<std::u16string> re1_1 = { u"arikitari", u"na", u"world!" };
+	const auto re1_2 = s1 | split(u' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
+	const std::u16string s2 = u"ありきたりな 世界";
+	const std::vector<std::u16string> re2_1 = { u"ありきたりな", u"世界" };
+	const auto re2_2 = s1 | split(u' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
 }
 IUTEST_TEST(SplitBySingeCharTest, type_char32_t) {
-	const std::u32string s = U"arikitari na world!";
-	const std::vector<std::u32string> re1 = { U"arikitari", U"na", U"world!" };
-	const auto re2 = s | split(U' ');
-	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
+	const std::u32string s1 = U"arikitari na world!";
+	const std::vector<std::u32string> re1_1 = { U"arikitari", U"na", U"world!" };
+	const auto re1_2 = s1 | split(U' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
+	const std::u32string s2 = U"ありきたりな 世界";
+	const std::vector<std::u32string> re2_1 = { U"ありきたりな", U"世界" };
+	const auto re2_2 = s1 | split(U' ');
+	IUTEST_ASSERT_TRUE(std::equal(re1_1.begin(), re1_1.end(), re1_2.begin(), re1_2.end()));
 }
 IUTEST_TEST(SplitCovertToIntBySingeCharTest, type_char) {
 	const std::string s = "123,421,113";
