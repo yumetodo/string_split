c358367d0fafda8ccf504d67369a94a0f447e545
yumetodo <yume-wikijp@live.jp>
2016-07-21 18:22:01 +0900

    add include gard(pragma once)

--- include/string_split.hpp
+++ include/string_split.hpp
@@ -4,6 +4,7 @@
   Distributed under the Boost Software License, Version 1.0.
   (See http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
+#pragma once
 #include <string>
 #include <vector>
 #include <type_traits>
