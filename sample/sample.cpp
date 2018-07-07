cf660a61b00f1ec1725752173fb1f51305dab0c0
yumetodo <yume-wikijp@live.jp>
2018-07-07 20:57:13 +0900

    feat(sample): add string_view sample

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -13,14 +13,25 @@ int main()
 	const auto s_1 = s | split(' ')[1];
 	if ("na" != s_1) return 1;
 	std::string s2 = "123,421,113";
+
+	//lamda or class contain operator()
 	const auto n1 = s2 | split(',') >> [](const std::string& s) {
 		return std::stoi(s);
-	};//lamda or class contain operator()
-	//const auto n2 = s2 | split(',') >> std::stoi;//std::stoi's prototype is int(const std::string&, std::size_t*, int), not int(const std::string&).
-	const auto n2 = s2 | split(',') >> to_int;//function-pointer
+	};
+
+	//std::stoi's prototype is int(const std::string&, std::size_t*, int), not int(const std::string&).
+	//const auto n2 = s2 | split(',') >> std::stoi;
+
+	//function-pointer
+	const auto n2 = s2 | split(',') >> to_int;
 	if (n1 != n2) return 2;
 	s2 | split(',') >> [](std::string&& s) {
 		std::cout << s << std::endl;
 	};
+
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+	std::string_view sw = "arikitari na sekai";
+	const auto r = sw | split(' ') >> back();
+#endif
 	return 0;
 }
