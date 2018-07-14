4ea5cbb4e27f49f306c87805ce50bc91fac378e5
yumetodo <yume-wikijp@live.jp>
2018-07-14 17:51:25 +0900

    feat(test): add missing string_view version test case

--- test/test_rvalue.cpp
+++ test/test_rvalue.cpp
@@ -109,6 +109,17 @@ IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_first_back_by_stl_str)
 		| split(s(constant::space_underscore<char_type>())) >> at_first().back();
 	IUTEST_ASSERT_EQ(constant::na_world<char_type>(), re);
 }
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_first_back_by_stl_str_view)
+{
+	using char_type = TypeParam;
+	using s = std::basic_string<char_type>;
+	using sw = std::basic_string_view<char_type>;
+	const auto re = s(constant::arikitari_na_world_underscore<char_type>())
+		| split(sw(constant::space_underscore<char_type>())) >> at_first().back();
+	IUTEST_ASSERT_EQ(constant::na_world<char_type>(), re);
+}
+#endif
 IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_first_by_single_char)
 {
 	using char_type = TypeParam;
@@ -136,6 +147,18 @@ IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_first_by_stl_str)
 	IUTEST_ASSERT_EQ(constant::arikitari<char_type>(), re[0]);
 	IUTEST_ASSERT_EQ(constant::na_world<char_type>(), re[1]);
 }
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_first_by_stl_str_view)
+{
+	using char_type = TypeParam;
+	using s = std::basic_string<char_type>;
+	using sw = std::basic_string_view<char_type>;
+	const auto re = s(constant::arikitari_na_world_underscore<char_type>()) | split(sw(constant::space_underscore<char_type>())) >> at_first();
+	IUTEST_ASSERT(2u == re.size());
+	IUTEST_ASSERT_EQ(constant::arikitari<char_type>(), re[0]);
+	IUTEST_ASSERT_EQ(constant::na_world<char_type>(), re[1]);
+}
+#endif
 IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_last_front_by_single_char)
 {
 	using char_type = TypeParam;
@@ -159,6 +182,17 @@ IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_last_front_by_stl_str)
 		| split(s(constant::space_underscore<char_type>())) >> at_last().front();
 	IUTEST_ASSERT_EQ(constant::arikitari_na<char_type>(), re);
 }
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_last_front_by_stl_str_view)
+{
+	using char_type = TypeParam;
+	using s = std::basic_string<char_type>;
+	using sw = std::basic_string_view<char_type>;
+	const auto re = s(constant::arikitari_na_world_underscore<char_type>())
+		| split(sw(constant::space_underscore<char_type>())) >> at_last().front();
+	IUTEST_ASSERT_EQ(constant::arikitari_na<char_type>(), re);
+}
+#endif
 IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_last_back)
 {
 	using char_type = TypeParam;
@@ -193,6 +227,18 @@ IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_last_by_stl_str)
 	IUTEST_ASSERT_EQ(constant::arikitari_na<char_type>(), re[0]);
 	IUTEST_ASSERT_EQ(constant::world<char_type>(), re[1]);
 }
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+IUTEST_TYPED_TEST(StringSplitRvalue, chain_at_last_by_stl_str_view)
+{
+	using char_type = TypeParam;
+	using s = std::basic_string<char_type>;
+	using sw = std::basic_string_view<char_type>;
+	const auto re = s(constant::arikitari_na_world_underscore<char_type>()) | split(sw(constant::space_underscore<char_type>())) >> at_last();
+	IUTEST_ASSERT(2u == re.size());
+	IUTEST_ASSERT_EQ(constant::arikitari_na<char_type>(), re[0]);
+	IUTEST_ASSERT_EQ(constant::world<char_type>(), re[1]);
+}
+#endif
 IUTEST_TYPED_TEST(StringSplitRvalue, ExtractBySingeChar)
 {
 	using char_type = TypeParam;
@@ -317,6 +363,22 @@ IUTEST_TYPED_TEST(StringSplitRvalue, SplitByStlStr)
 	IUTEST_ASSERT_TRUE(std::equal(std::begin(re1_1), std::end(re1_1), re1_2.begin(), re1_2.end()));
 	StringSplitRvalue_SplitByStlStr::without_char<char_type>();
 }
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+IUTEST_TYPED_TEST(StringSplitRvalue, SplitByStlStrView)
+{
+	using char_type = TypeParam;
+	using s = std::basic_string<char_type>;
+	using sw = std::basic_string_view<char_type>;
+	const s re1_1[] = { constant::arikitari<char_type>(), constant::na<char_type>(), constant::world<char_type>() };
+	const auto re1_2 = s(constant::arikitari_na_world_underscore<char_type>()) | split(sw(constant::space_underscore<char_type>()));
+	IUTEST_ASSERT_TRUE(std::equal(std::begin(re1_1), std::end(re1_1), re1_2.begin(), re1_2.end()));
+	if constexpr(!std::is_same_v<char_type, char>) {
+		const s re2_1[] = { constant::arikitarina<char_type>(), constant::sekai<char_type>() };
+		const auto re2_2 = s(constant::arikitarina_sekai_wspace<char_type>()) | split(sw(constant::wspace<char_type>()));
+		IUTEST_ASSERT_TRUE(std::equal(std::begin(re2_1), std::end(re2_1), re2_2.begin(), re2_2.end()));
+	}
+}
+#endif
 template<typename T>
 struct StringSplitRvalueCovertToInt : public ::iutest::Test {};
 IUTEST_TYPED_TEST_CASE(StringSplitRvalueCovertToInt, ::iutest::Types<char, wchar_t>);
@@ -350,6 +412,19 @@ IUTEST_TYPED_TEST(StringSplitRvalueCovertToInt, ByStlStr)
 	};
 	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
 }
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+IUTEST_TYPED_TEST(StringSplitRvalueCovertToInt, ByStlStrView)
+{
+	using char_type = TypeParam;
+	using s = std::basic_string<char_type>;
+	using sw = std::basic_string_view<char_type>;
+	constexpr std::array<int, 3> re1{ { 123, 421, 113 } };
+	const auto re2 = s(constant::csv_data_123_421_113_with_space<char_type>()) | split(sw(constant::space_comma<char_type>())) >> [](const s& s) {
+		return std::stoi(s);
+	};
+	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
+}
+#endif
 IUTEST_TYPED_TEST(StringSplitRvalueCovertToInt, NoReturnBySingeChar)
 {
 	using char_type = TypeParam;
@@ -386,3 +461,18 @@ IUTEST_TYPED_TEST(StringSplitRvalueCovertToInt, NoReturnByStlStr)
 	};
 	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
 }
+#ifdef STRING_SPLIT_HAS_CXX17_STRING_VIEW
+IUTEST_TYPED_TEST(StringSplitRvalueCovertToInt, NoReturnByStlStrView)
+{
+	using char_type = TypeParam;
+	using s = std::basic_string<char_type>;
+	using sw = std::basic_string_view<char_type>;
+	constexpr std::array<int, 3> re1{ { 123, 421, 113 } };
+	std::vector<int> re2;
+	re2.reserve(3);
+	s(constant::csv_data_123_421_113_with_space<char_type>()) | split(sw(constant::space_comma<char_type>())) >> [&re2](const s& s) {
+		re2.push_back(std::stoi(s));
+	};
+	IUTEST_ASSERT_TRUE(std::equal(re1.begin(), re1.end(), re2.begin(), re2.end()));
+}
+#endif
